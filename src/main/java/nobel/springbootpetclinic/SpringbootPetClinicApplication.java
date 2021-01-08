b0c3e503b4e79c8fa82f92245b1f243200aa77b3
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-08 17:00:22 +0200

    first commit

---
+++ src/main/java/nobel/springbootpetclinic/SpringbootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package nobel.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nobel/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package nobel.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

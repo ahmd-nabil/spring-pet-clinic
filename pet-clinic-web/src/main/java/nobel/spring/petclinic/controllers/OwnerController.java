8f60583246a38fcb8826f1c8adb1b199f3886121
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-12 10:58:06 +0200

    Implemented Owners Index page and Controller. closes #15

---
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package nobel.spring.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}

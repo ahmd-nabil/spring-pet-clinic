75e3facdf75812206a8bbe9f6431c81dfbe88916
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-12 10:31:27 +0200

    Implemented Index page and IndexController. closes #8

---
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package nobel.spring.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String indexPage() {
+        return "index";
+    }
+}

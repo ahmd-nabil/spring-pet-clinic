89d056736263d6ae4a7fc6368be1f7662fcd9767
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-17 18:26:18 +0200

    Added vets list and updated VetController. closes #14

--- pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package nobel.spring.petclinic.controllers;
 
+import nobel.spring.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }

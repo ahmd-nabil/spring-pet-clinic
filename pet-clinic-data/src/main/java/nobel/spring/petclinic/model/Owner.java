e0801a8f9413421adba7ef917cfc8a8a4ce91bf0
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-19 20:03:44 +0200

    Added contact info to Owner. closes #28

--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
@@ -3,7 +3,35 @@ package nobel.spring.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+
+    private String Address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;

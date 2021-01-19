bd33ff2c177cb993f384634c36d7733cfe914424
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-19 20:00:19 +0200

    Added Specialty and Associated to Vet. closes #27

--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package nobel.spring.petclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
 
+public class Vet extends Person{
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Visit.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 public class Visit extends BaseEntity{
     private LocalDate localDate;
     private String description;
-        private Pet pet;
+    private Pet pet;
 
     public LocalDate getLocalDate() {
         return localDate;

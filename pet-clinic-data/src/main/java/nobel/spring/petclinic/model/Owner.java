215efeb7c90c6bb70432018ecdc8aeaef30530e0
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-19 19:55:13 +0200

    Added visits, Updated Owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
@@ -1,5 +1,15 @@
 package nobel.spring.petclinic.model;
 
-public class Owner extends Person{
+import java.util.Set;
 
+public class Owner extends Person{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Visit.java
@@ -0,0 +1,34 @@
+package nobel.spring.petclinic.model;
+
+import java.time.LocalDate;
+
+
+public class Visit extends BaseEntity{
+    private LocalDate localDate;
+    private String description;
+        private Pet pet;
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}

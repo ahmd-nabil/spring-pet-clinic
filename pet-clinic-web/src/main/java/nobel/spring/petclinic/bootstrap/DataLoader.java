17da151c576237d7e69e53549172df4a51f92c05
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-19 20:31:00 +0200

    Loaded some PetTypes in bootstrap. closes #32

--- pet-clinic-web/src/main/java/nobel/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package nobel.spring.petclinic.bootstrap;
 
 import nobel.spring.petclinic.model.Owner;
+import nobel.spring.petclinic.model.PetType;
 import nobel.spring.petclinic.model.Vet;
 import nobel.spring.petclinic.services.OwnerService;
+import nobel.spring.petclinic.services.PetTypeService;
 import nobel.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType type1 = new PetType();
+        type1.setName("Dog");
+        PetType savedDogType = petTypeService.save(type1);
+
+        PetType type2 = new PetType();
+        type2.setName("Cat");
+        PetType savedCatType = petTypeService.save(type2);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Ahmed");
         owner1.setLastName("Nabil");

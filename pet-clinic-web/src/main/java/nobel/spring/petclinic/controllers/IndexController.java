c156621c4957610f5bb15676790f331c9a3978f3
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-19 18:17:10 +0200

    Implemented Master layout for index page. closes #19

--- pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package nobel.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
     public String indexPage() {
         return "index";
+    }
+
+    @GetMapping("/oups")
+    public String error() {
+        return "error";
     }
 }

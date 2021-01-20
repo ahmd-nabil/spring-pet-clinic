bcb34153e850dcd7afe2a45fa47b065338e78cdf
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-20 20:53:06 +0200

    Fixed nav-bar broken links

--- pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/OwnerController.java
@@ -3,6 +3,7 @@ package nobel.spring.petclinic.controllers;
 import nobel.spring.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,10 @@ public class OwnerController {
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/find")
+    public String findOwner() {
+        return "error";
     }
 }

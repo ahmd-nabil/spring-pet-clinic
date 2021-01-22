8d81bceabfbfce5d109b96bb36fdb7d9071bd7f1
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-22 20:12:30 +0200

    Updated Owner to JPA Entity and Added some relationships to other Entities. closes #36

--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package nobel.spring.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "address")
     private String Address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Person.java
@@ -1,9 +1,15 @@
 package nobel.spring.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {

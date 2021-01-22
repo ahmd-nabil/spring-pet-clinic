4a38ae89ba23dc7442d4621bc6fedf387e3fcfef
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-22 21:20:59 +0200

    Added spring data jpa service for Vet. closes #41

---
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package nobel.spring.petclinic.services.springdatajpa;
+
+import nobel.spring.petclinic.model.Vet;
+import nobel.spring.petclinic.repositories.VetRepository;
+import nobel.spring.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}

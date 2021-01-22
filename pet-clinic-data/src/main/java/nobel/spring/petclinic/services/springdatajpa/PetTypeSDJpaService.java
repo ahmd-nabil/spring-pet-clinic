7cf7e5f08904eaf10df51f622e6d0d57fcea2947
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-22 21:25:25 +0200

    Added spring data jpa service for PetTypes. closes #42

---
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package nobel.spring.petclinic.services.springdatajpa;
+
+import nobel.spring.petclinic.model.PetType;
+import nobel.spring.petclinic.repositories.PetTypeRepository;
+import nobel.spring.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}

55b2792b10dc499b1f50ae6d939dd8c23d8a87ad
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-01-22 20:23:53 +0200

    Updated Vet to JPA Entity. closes #37

--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Specialty.java
@@ -1,7 +1,19 @@
 package nobel.spring.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
+    @Column(name = "description")
     private String description;
+
+    @ManyToMany(mappedBy = "specialties")
+    private Set<Vet> vets;
 
     public String getDescription() {
         return description;
@@ -9,5 +21,14 @@ public class Specialty extends BaseEntity{
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+
+    public Set<Vet> getVets() {
+        return vets;
+    }
+
+    public void setVets(Set<Vet> vets) {
+        this.vets = vets;
     }
 }
--- pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/nobel/spring/petclinic/model/Vet.java
@@ -1,13 +1,26 @@
 package nobel.spring.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+    @ManyToMany(fetch = FetchType.EAGER)        //default fetch type is LAZY for @ManyToMany
+    @JoinTable(
+            name = "vet_specialty",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
         return specialties;
+    }
+
+    public void addSpecialty(Specialty specialty) {
+        this.specialties.add(specialty);
     }
 
     public void setSpecialties(Set<Specialty> specialties) {

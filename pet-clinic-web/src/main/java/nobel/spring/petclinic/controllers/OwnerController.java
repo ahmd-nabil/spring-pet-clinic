699b3a2b716be6be819f0306579b53b973d55b0b
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-06-20 14:43:27 +0200

    Add Display Owner Details.

--- pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/nobel/spring/petclinic/controllers/OwnerController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -51,5 +53,10 @@ public class OwnerController {
         }
     }
 
-
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
 }
